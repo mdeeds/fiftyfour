56c20d626d042f82d19f34d5739387a6b4f08b4e
Matt Deeds <mdeeds@gmail.com>
2021-04-28 16:48:51 -0700

    Add names.

--- ts/player.ts
+++ ts/player.ts
@@ -8,12 +8,19 @@ export class Player {
     }
     return players;
   }
+
+  private static names = ['Abigail', 'Bob', 'Charlotte', 'Doogle', 'Eddie',
+    'Francine', 'Gary', 'Herman', 'Izzie', 'Jack', 'Kili'];
+  private static nameIndex = 0;
+
   readonly name: string;
   readonly holeCards: Array<Card> = new Array<Card>();
   public chips: number;
   readonly isFolded: boolean;
 
   constructor() {
+    this.name = Player.names[Player.nameIndex];
+    ++Player.nameIndex;
     this.isFolded = false;
   }
 }
